a89169b403a6bba1bce126b45d75586fcb23b3cd
debojyoti <debojyoti.bose@cognizant.com>
2021-11-15 12:22:52 +0530

    Added test.php & modified bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-packages=( inotify-tools php libapache2-mod-php )
+packages=( inotify-tools apache2 php libapache2-mod-php )
 CRON_FILE="/var/spool/cron/crontabs/root"
 CHECK_CONFIGURATION="/root/config_Check.sh"
+APACHE_CONFIG="/etc/apache2/apache2.conf"
+HTACCESS="/var/www/html/.htaccess"
+DOCROOT="/var/www/html/"
 
 configure_Apache(){
-	cp test.php /var/www/html/
-	echo "DirectoryIndex test.php" >> /var/www/html/.htaccess
-	sed  -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
-	echo "ServerName localhost" >>  /etc/apache2/apache2.conf
+	cp test.php "$DOCROOT"
+	echo "DirectoryIndex test.php" >> "$HTACCESS"
+	sed  -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/' "$APACHE_CONFIG"
+	echo "ServerName localhost" >>  "$APACHE_CONFIG"
+	echo "Restarting Apache service..."
+        systemctl restart apache2
 }
 
 for package in "${packages[@]}"
@@ -16,16 +21,20 @@ do
   if [ $? -ne 0 ]; then
   	echo "Installing $package..."
 	apt install -y "$package"
-	if [[ "$package" == "libapache2-mod-php" ]];then
-		#Get the Apache version
-		export apache_version=$(apache2 -version| grep -i version| awk -F '/' '{print $2}' | awk '{print $1}')
-		#Configuring Apache
-		configure_Apache
-		echo "Restarting Apache service..."
-		systemctl restart apache2
 
+	if [[ "$package" == "libapache2-mod-php" ]] ||  [[ "$package" == "apache2" ]];then	
+		#Set the Apache version as env variable
+		export apache_version=$(apache2 -version| grep -i version| awk -F '/' '{print $2}' | awk '{print $1}')
+		#Configure Apache if apache2.conf and .htaccess is absent
+		if [ ! -f $APACHE_CONFIG ] && [ ! -f $HTACCESS ];then
+		   echo "Configuring Apache..."
+	           configure_Apache
+		else
+		   echo "Restarting Apache service..."
+		   systemctl restart apache2
+		fi
 	elif [[ "$package" == "php" ]]; then
-		#Get the PHP version
+		#Set the PHP version as env variable
 		export php_version=$(php -v| grep -i "^PHP"| awk '{print $2}'| awk -F '-' '{print $1}')
 	fi	
   else
@@ -42,7 +51,7 @@ fi
 # Set a cron entry for config_Check.sh
 grep -qi "$CHECK_CONFIGURATION" $CRON_FILE
 	if [ $? != 0 ]; then
-	   echo "Updating cron job"
+	   echo "Updating cron job..."
            /bin/echo "* * * * * /bin/bash $CHECK_CONFIGURATION >/root/configure.log 2>&1" >> $CRON_FILE
 	   systemctl restart cron.service
 	fi
